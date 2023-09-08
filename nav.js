e35337e748b2464f79c0bce252f5e5d765260049
Quollian <9434180-Quollian@users.noreply.replit.com>
2023-09-08 03:55:40 +0000

    add pretty

---
+++ nav.js
@@ -0,0 +1,17 @@
+function normalNav(active) {
+
+  nav = document.getElementById('navigation');
+  
+  
+
+  nav.innerHTML= '<nav id="list"><ul><li><a id="home" href="/index.html">TRANSLATOR</a></li><li><a id="sudoku" href="/sudoku.html">SUDOKU</a></li><li><a id="scramble" href="/scramble.html" >SCRAMBLE</a></li><li><a id="wordsearch" href="/wordsearch.html">WORDSEARCH</a></li><li><a id="cryptogram" href="/cryptogram.html">CRYPTOGRAM</a></li><li style="float:right" id="title"><a href="/index.html">Erostian Games</a></li></ul></nav>';
+
+  
+  // add active classes
+  for (let i = 0; i < active.length; i++){
+    current = document.getElementById(active[i]);
+    current.classList.add("active");
+  }
+
+  
+}
