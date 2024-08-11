4a39cac31b009220ba31c8fe55532f92f21e4d26
Quollian <quollian@gmail.com>
2024-08-11 01:18:08 +0000

    Commit to git

--- nav.js
+++ nav.js
@@ -4,7 +4,7 @@ function normalNav(active) {
   
   
 
-  nav.innerHTML= '<nav id="list"><ul><li><a id="home" href="/index.html">TRANSLATOR</a></li><li><a id="sudoku" href="/sudoku.html">SUDOKU</a></li><li><a id="scramble" href="/scramble.html" >SCRAMBLE</a></li><li><a id="wordsearch" href="/wordsearch.html">WORDSEARCH</a></li><li><a id="cryptogram" href="/cryptogram.html">CRYPTOGRAM</a></li><li style="float:right" id="title"><a href="/index.html">Erostian Games</a></li></ul></nav>';
+  nav.innerHTML= '<nav id="list"><ul><li><a id="home" href="/index.html">TRANSLATOR</a></li><li><a id="sudoku" href="/sudoku.html">SUDOKU</a></li><li><a id="scramble" href="/scramble.html" >SCRAMBLE</a></li><li><a id="wordsearch" href="/wordsearch.html">WORDSEARCH</a></li><li><a id="cryptogram" href="/cryptogram.html">CRYPTOGRAM</a></li><li><a id="hangman" href="/hangumi.html">HANGMAN</a></li><li style="float:right" id="title"><a href="/index.html">Erostian Games</a></li></ul></nav>';
 
   
   // add active classes
